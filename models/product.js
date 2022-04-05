ee6cf734ffb2228d6d5420f5597a1325caf77cf9
Mdbshero <mdbshero@gmail.com>
2022-04-04 20:13:12 -0500

    added joi to validate products.

--- models/product.js
+++ models/product.js
@@ -2,6 +2,7 @@
 //TODO: Create a model from that schema
 
 const mongoose = require("mongoose");
+const Joi = require("joi")
 
 const productSchema = new mongoose.Schema({
     name: {type: String, required: true, minlength: 2, max: 255},
@@ -11,6 +12,18 @@ const productSchema = new mongoose.Schema({
     dateAdded: {type: Date, default:Date.now()}
 });
 
+function validateProduct(product){
+    const schema = Joi.object({
+        name: Joi.string().min(2).max(255).required(),
+        description: Joi.string().required(),
+        category: Joi.string().min(2).max(255).required(),
+        price: Joi.number().required()
+    })
+    return schema.validate(product);
+}
 const Product = mongoose.model('Product', productSchema);
 
-module.exports = Product;
+module.exports = {
+    Product,
+    validateProduct
+};
--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,24 @@
-const product = require("../models/product");
 const express = require("express");
+const { valid } = require("joi");
+const { Product, validateProduct} = require("../models/product");
 const router = express.Router();
 
 //Endpoints go here
+router.post("/", async (req, res) => {
+    try {
+        const {error} = validateProduct(req.body);
+        if (error) return res.status(400).send(error);
+
+        let newProduct = await new Product(req.body);
+        await newProduct.save();
+
+        return res.status(201).send(newProduct);
+        
+    } catch (error) {
+
+        return res.status(500).send(`Internal Server Error: ${error}`)
+    }
+});
 
 
 
